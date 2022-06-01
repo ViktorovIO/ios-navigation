eaa356e0f0f7484e0c7d2fbe4f93f582d7dc5a57
Игорь Викторов <viktorov@MacBook-Pro-Igor.local>
2022-06-02 00:57:00 +0500

    iosui-2.1 make homework

---
+++ ios-navigation/ios-navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,101 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Игорь Викторов on 27.05.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    var avatarImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "avatar"))
+        imageView.layer.cornerRadius = 75
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+
+    private lazy var fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Igor Viktorov"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Статус профиля"
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isUserInteractionEnabled = true
+
+        return label
+    }()
+
+    private lazy var setStatusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Установить статус", for: .normal)
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        button.addTarget(self, action: #selector(self.setStatusButtonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.systemBlue
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowOpacity = 0.7
+        button.layer.shadowRadius = 4.0
+        button.layer.masksToBounds = false
+        
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.drawSelf()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func drawSelf() {
+        addSubview(avatarImageView)
+        addSubview(fullNameLabel)
+        addSubview(statusLabel)
+        addSubview(setStatusButton)
+
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 150),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 150),
+            
+            fullNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 27),
+            fullNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 182),
+            fullNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16),
+            setStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            setStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            setStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            statusLabel.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -34),
+            statusLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            statusLabel.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor)
+        ])
+    }
+    
+    @objc private func setStatusButtonPressed() {
+        print(statusLabel)
+    }
+}
--- ios-navigation/ios-navigation/Profile/ProfileViewController.swift
+++ ios-navigation/ios-navigation/Profile/ProfileViewController.swift
@@ -2,7 +2,7 @@
 //  ProfileViewController.swift
 //  Navigation
 //
-//  Created by Игорь Викторов on 14.05.2022.
+//  Created by Игорь Викторов on 27.05.2022.
 //
 
 import UIKit
@@ -11,5 +11,30 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        title = "Profile"
+        view.backgroundColor = .lightGray
+
+        // ProfileHeaderView
+        let profileHeaderView = ProfileHeaderView(frame: .zero)
+        profileHeaderView.backgroundColor = .white
+        view.addSubview(profileHeaderView)
+
+        let barButtonItem = UIBarButtonItem(title: "Запись", style: .plain, target: self, action: #selector(showPostInfo))
+        navigationItem.rightBarButtonItem = barButtonItem
+
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.barTintColor = UIColor.white
+    }
+    
+    @objc func showPostInfo() {
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        view.subviews.first?.frame = CGRect(x: 0, y: 0, width: super.view.frame.width, height: super.view.frame.height)
     }
 }
