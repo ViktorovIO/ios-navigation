23b91f73dde4bb20c417ab4830d08ab3e6c96457
ViktorovIO <viktorovio@yahoo.com>
2022-09-28 15:01:35 +0500

    #iosint-1 make homework

--- ios-navigation/StorageService/Post.swift
+++ ios-navigation/StorageService/Post.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-struct Post {
-    var author, description, image: String
-    var likes, views: Int
+public struct Post {
+    public var author, description, image: String
+    public var likes, views: Int
     
-    init(author: String, description: String, image: String, likes: Int, views: Int) {
+    public init(author: String, description: String, image: String, likes: Int, views: Int) {
         self.author = author
         self.description = description
         self.image = image
--- ios-navigation/ios-navigation/Post/PostViewController.swift
+++ ios-navigation/ios-navigation/Post/PostViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import StorageService
 
 class PostViewController: UIViewController {
     var post: Post
